62791d00e62742e86ea92d216491ddf582c1a103
UniqueMc <160955149+UniqueMc@users.noreply.github.com>
2025-05-01 03:45:42 -0400

    favorites.ts

---
+++ src/types/favorites.ts
@@ -0,0 +1,10 @@
+export interface Stock {
+  ticker: string;
+  name: string;
+  industry: string;
+  sector: string;
+  price: number;
+  marketCap: number;
+  volume: number;
+  lastUpdated: string;
+} 
