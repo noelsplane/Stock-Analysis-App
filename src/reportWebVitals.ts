195b9f8e76dd2f096ca37b5288ba9ee729df4d55
UniqueMc <160955149+UniqueMc@users.noreply.github.com>
2025-05-02 13:18:37 -0400

    reportWebVitals.ts

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,15 @@
+import type { ReportHandler } from 'web-vitals';
 
-import webVitals from 'web-vitals';
-
-const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    webVitals.getCLS(onPerfEntry);
-    webVitals.getFID(onPerfEntry);
-    webVitals.getFCP(onPerfEntry);
-    webVitals.getLCP(onPerfEntry);
-    webVitals.getTTFB(onPerfEntry);
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
   }
 };
 
 export default reportWebVitals;
-
