073c8cf3d8ac88d2a45b4df7b770bef987ed158d
UniqueMc <160955149+UniqueMc@users.noreply.github.com>
2025-05-01 23:34:19 -0400

    LoadingComponent.tsx

---
+++ src/components/LoadingComponent.tsx
@@ -0,0 +1,8 @@
+// src/components/LoadingComponent.tsx
+import React from 'react';
+
+const LoadingComponent: React.FC = () => {
+  return <div className="loading">Loading stock data...</div>;
+};
+
+export default LoadingComponent;
