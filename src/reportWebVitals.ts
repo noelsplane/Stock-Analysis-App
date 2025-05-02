edc955179264684a084a5ffe438f49874901873a
UniqueMc <160955149+UniqueMc@users.noreply.github.com>
2025-05-01 21:50:59 -0400

    reportWebVitals.ts

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,13 +1,13 @@
+// src/reportWebVitals.ts
+import { ReportHandler, getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
 
-import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
-
-const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    onCLS(onPerfEntry);
-    onFID(onPerfEntry);
-    onFCP(onPerfEntry);
-    onLCP(onPerfEntry);
-    onTTFB(onPerfEntry);
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
