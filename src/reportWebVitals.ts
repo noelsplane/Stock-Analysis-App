4fa567a1c4aefd424416d81bdc06bd828ce21dfe
UniqueMc <160955149+UniqueMc@users.noreply.github.com>
2025-05-01 23:19:07 -0400

    reportWebVitals.ts

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,17 +1,15 @@
-// src/reportWebVitals.ts
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
 
-// Correct type for onPerfEntry is `ReportHandler` which is the default export
-type ReportHandler = (metric: { name: string; value: number }) => void;
+import webVitals from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    getCLS(onPerfEntry);
-    getFID(onPerfEntry);
-    getFCP(onPerfEntry);
-    getLCP(onPerfEntry);
-    getTTFB(onPerfEntry);
+    webVitals.getCLS(onPerfEntry);
+    webVitals.getFID(onPerfEntry);
+    webVitals.getFCP(onPerfEntry);
+    webVitals.getLCP(onPerfEntry);
+    webVitals.getTTFB(onPerfEntry);
   }
 };
 
 export default reportWebVitals;
+
